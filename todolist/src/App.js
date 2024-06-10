ceba908af2904396c60c6650bfc588277a5a64d5
minyoung <myoung.ever@gmail.com>
2024-06-11 01:58:31 +0900

    ✨ Feat: add TodoContext component
    - TodoContext 컴포넌트 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { createGlobalStyle } from 'styled-components';
 
@@ -6,6 +6,7 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -15,14 +16,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
