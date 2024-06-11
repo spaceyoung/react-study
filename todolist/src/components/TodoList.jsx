c2d6d242ca5d4cebffb4726cbda2a70ef69775f9
minyoung <myoung.ever@gmail.com>
2024-06-11 22:44:33 +0900

    🐛 Fix: fix TodoItem component name
    - TodoItem 컴포넌트 이름 수정

--- todolist/src/components/TodoList.jsx
+++ todolist/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import TodoItem from './TodoLitem';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
